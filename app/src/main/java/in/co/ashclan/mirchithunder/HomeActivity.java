30ca15e57dc86f69c90bf1e3e55e546f8f9067db
AshClan <aashaykarekar@gmail.com>
2018-07-04 12:56:34 +0530

    commit

--- app/src/main/java/in/co/ashclan/mirchithunder/HomeActivity.java
+++ app/src/main/java/in/co/ashclan/mirchithunder/HomeActivity.java
@@ -1,13 +1,26 @@
 package in.co.ashclan.mirchithunder;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+    Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mInit();
+        Log.e("--->MEMBER>> ","");
+        Toast.makeText(mContext,"",Toast.LENGTH_LONG).show();
+    }
+
+
+    private void mInit(){
+        mContext = HomeActivity.this;
+
     }
 }
