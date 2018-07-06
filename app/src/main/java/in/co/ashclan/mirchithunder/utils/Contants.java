15df63683eea6a0e3f15a7fe00b68d06630b3b90
AshClan <aashaykarekar@gmail.com>
2018-07-06 12:55:04 +0530

    commit Aashay

---
+++ app/src/main/java/in/co/ashclan/mirchithunder/utils/Contants.java
@@ -0,0 +1,8 @@
+package in.co.ashclan.mirchithunder.utils;
+
+public class Contants {
+
+    public static final String FIREBASE_DATABASE="https://radiomirchi-418f4.firebaseio.com/";
+    public static final String FIREBASE_STROAGE="gs://radiomirchi-418f4.appspot.com";
+
+}
